7476305ed6599b8fc0f433a33e8aeb39a3f11cc7
MSANDA1998 <mohammedmsanda@gmail.com>
2020-03-11 16:17:34 +0100

    commit branch 2

--- src/Surfaces.java
+++ src/Surfaces.java
@@ -2,8 +2,8 @@ public class Surfaces{
     public static void main(String[]args){
         int a=2;
         int b=3;
-        int c= a*b;
-        System.out.println(c);
+        int T= (a*b)/2;
+        System.out.println(T);
     }
 }
 
